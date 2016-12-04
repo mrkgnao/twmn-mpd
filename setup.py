d437b1e3ad2db79ee40ee3590b2115d65db50349
Soham Chowdhury <chow.soham@gmail.com>
2016-12-04 10:59:47 +0530

    Fix module search error

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='twmn_mpd',
-      version='0.1.3',
+      version='0.1.4',
       description='A simple mpd notifier for twmn',
       url='http://github.com/mrkgnao/twmn-mpd',
       author='Soham Chowdhury',
       author_email='chow.soham@gmail.com',
       license='MIT',
-      packages=['twmn_mpd'],
+      packages=['twmn_mpd', 'twmn_mpd.providers'],
       install_requires=[
           'python-mpd2'
       ],
