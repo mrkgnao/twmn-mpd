d6ea62b19977b937a1d0f2944ed306ed815b8735
Soham Chowdhury <chow.soham@gmail.com>
2016-12-04 10:14:44 +0530

    Updated main.py to refer to new providers module

--- twmn_mpd/main.py
+++ twmn_mpd/main.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import socket
 from contextlib import closing
 
 import twmn_mpd.MsgQueue as MsgQueue
-from twmn_mpd.NowPlayingNotifier import NowPlayingNotifier
-from twmn_mpd.PlayStateNotifier import PlayStateNotifier
+from twmn_mpd.providers.NowPlayingNotifier import NowPlayingNotifier
+from twmn_mpd.providers.PlayStateNotifier import PlayStateNotifier
 
 
 def create_notifs(sock, notifiers):
@@ -20,9 +17,9 @@ def create_notifs(sock, notifiers):
         sock.send(msg.encode())
 
 
-def main(ip="127.0.0.1",
-         port=9797,
-         notifiers=[PlayStateNotifier, NowPlayingNotifier]):
+def start_notifs(ip="127.0.0.1",
+                 port=9797,
+                 notifiers=[PlayStateNotifier, NowPlayingNotifier]):
     """
     The main entrypoint to twmn_mpd.
     """
@@ -31,7 +28,3 @@ def main(ip="127.0.0.1",
                                socket.SOCK_DGRAM)) as twmn_sock:
         twmn_sock.connect((ip, port))
         create_notifs(twmn_sock, notifiers)
-
-
-if __name__ == '__main__':
-    main()
